c0d78c7016a40cd211a267eb325a049581feed23
Mohamed Hamada <mohamedhamada@gmail.com>
2026-02-08 16:58:32 +0200

    manage ui

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,22 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:folder/helper/show_snack_bar.dart';
 import 'package:folder/pages/chat_page.dart';
+import 'package:folder/pages/cubits/register_cubit.dart';
+import 'package:folder/pages/cubits/register_state.dart';
 import 'package:folder/widget/constant.dart';
 import 'package:folder/widget/custom_button.dart';
 import 'package:folder/widget/custom_text_field.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class RegisterPage extends StatefulWidget {
-  const RegisterPage({super.key});
-  static String id = 'RegisterPage';
-
-  @override
-  State<RegisterPage> createState() => _RegisterPageState();
-}
-
-class _RegisterPageState extends State<RegisterPage> {
+class RegisterPage extends StatelessWidget {
   String? email;
+  static String id = 'RegisterPage';
 
   String? password;
 
@@ -26,110 +22,126 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: isLoading,
-      child: Scaffold(
-        backgroundColor: kPrimaryColor,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Form(
-            key: formKey,
-            child: ListView(
-              children: [
-                SizedBox(height: 75),
-                Image.asset(kLogo, height: 100),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'ScolarChat',
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontFamily: 'Pacifico',
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 75),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Register',
-                      style: TextStyle(fontSize: 24, color: Colors.white),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                CustomTextFormField(
-                  onChanged: (data) {
-                    email = data;
-                  },
-                  hintText: 'Email',
-                ),
-                SizedBox(height: 10),
-                CustomTextFormField(
-                  onChanged: (data) {
-                    password = data;
-                  },
-                  hintText: 'Password',
-                ),
-                SizedBox(height: 20),
-                CustomButton(
-                  onTap: () async {
-                    if (formKey.currentState!.validate()) {
-                      isLoading = true;
-                      setState(() {});
-                      try {
-                        await registerUser();
-                        Navigator.pushNamed(context, ChatPage.id , arguments: email);
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'weak-password') {
-                          showSnachBar(
-                            context,
-                            'The password provided is too weak.',
-                          );
-                        } else if (e.code == 'email-already-in-use') {
-                          showSnachBar(
-                            context,
-                            'The account already exists for that email.',
-                          );
-                        }
-                      } catch (e) {
-                        showSnachBar(context, 'there was an error');
-                      }
-                      isLoading = false;
-                      setState(() {});
-                    } else {}
-                  },
-                  title: 'Register',
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Don't have an account?",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(width: 5),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text(
-                        'Login',
+    return BlocListener<RegisterCubit, RegisterState>(
+      listener: (context, state) {
+        if (state is RegisterLoading) {
+          isLoading = true;
+        } else if (state is RegisterSuccess) {
+          isLoading = false;
+          Navigator.pushNamed(context, ChatPage.id, arguments: email);
+        } else if (state is RegisterFailure) {
+          isLoading = false;
+          showSnachBar(context, state.errorMessage);
+        }
+      },
+      child: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Scaffold(
+          backgroundColor: kPrimaryColor,
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Form(
+              key: formKey,
+              child: ListView(
+                children: [
+                  SizedBox(height: 75),
+                  Image.asset(kLogo, height: 100),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'ScolarChat',
                         style: TextStyle(
+                          fontSize: 32,
+                          fontFamily: 'Pacifico',
                           color: Colors.white,
-                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                  SizedBox(height: 75),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Register',
+                        style: TextStyle(fontSize: 24, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  CustomTextFormField(
+                    onChanged: (data) {
+                      email = data;
+                    },
+                    hintText: 'Email',
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextFormField(
+                    onChanged: (data) {
+                      password = data;
+                    },
+                    hintText: 'Password',
+                  ),
+                  SizedBox(height: 20),
+                  CustomButton(
+                    onTap: () async {
+                      if (formKey.currentState!.validate()) {
+                        isLoading = true;
+
+                        try {
+                          await registerUser();
+                          Navigator.pushNamed(
+                            context,
+                            ChatPage.id,
+                            arguments: email,
+                          );
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'weak-password') {
+                            showSnachBar(
+                              context,
+                              'The password provided is too weak.',
+                            );
+                          } else if (e.code == 'email-already-in-use') {
+                            showSnachBar(
+                              context,
+                              'The account already exists for that email.',
+                            );
+                          }
+                        } catch (e) {
+                          showSnachBar(context, 'there was an error');
+                        }
+                        isLoading = false;
+                      } else {}
+                    },
+                    title: 'Register',
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(width: 5),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),

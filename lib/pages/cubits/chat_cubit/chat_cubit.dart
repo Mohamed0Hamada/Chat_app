5d7be4c4868e40dda39aee08bb0ee24845d9b882
Mohamed Hamada <mohamedhamada@gmail.com>
2026-02-09 16:13:30 +0200

    create function send message

--- lib/pages/cubits/chat_cubit/chat_cubit.dart
+++ lib/pages/cubits/chat_cubit/chat_cubit.dart
@@ -1,6 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:folder/pages/cubits/chat_cubit/chat_state.dart';
+import 'package:folder/widget/constant.dart';
 
 class ChatCubit extends Cubit<ChatState> {
   ChatCubit() : super(ChatInitial());
+  CollectionReference messages = FirebaseFirestore.instance.collection(
+    kMessageCollection,
+  );
+  void sendMessage({required String message, required String email}) {
+    messages.add({kMessage: message, kCreatedAt: DateTime.now(), 'id': email});
+  }
 }

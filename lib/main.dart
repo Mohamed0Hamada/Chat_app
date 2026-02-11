4208c87cada81968848fadee094956e6df0fcfad
Mohamed Hamada <mohamedhamada@gmail.com>
2026-02-11 15:29:43 +0200

    create simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async' as Blocoverrides;
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,8 +14,9 @@ import 'package:folder/pages/register_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  runApp(const ScholarChat());
+  Blocoverrides.runZoned(() {
+    runApp(const ScholarChat());
+  });
 }
 
 class ScholarChat extends StatelessWidget {
